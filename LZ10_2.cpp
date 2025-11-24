86801e8a78a94fd11704e8772f31b1f434020d75
nastushal1102-oss <nastushal1102@gmail.com>
2025-11-24 15:41:01 +0200

    Add files via upload

---
+++ LZ10_2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <windows.h>
+
+using namespace std;
+
+// Функція обчислення суми
+double calculateSum(double x) {
+    double S = 0.0;
+
+    // Сума від k = 2 до 6
+    for (int k = 2; k <= 6; k++) {
+        S += sin(0.17 * pow(x, k)) / (2 * k + x);
+    }
+
+    return S;
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double x;
+
+    cout << "Введіть значення x: ";
+    cin >> x;
+
+    // Обчислення суми
+    double result = calculateSum(x);
+
+    // Виведення результату
+    cout << "Результат обчислення суми: "
+        << fixed << setprecision(6) << result << endl;
+
+    return 0;
+}
+
